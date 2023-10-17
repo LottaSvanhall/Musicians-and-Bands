4279ff4aa23e224076379818f992b45d9287dad9
LottaSvanhall <charlotte.svanhall@live.se>
2023-10-17 19:11:33 +0200

    Updated menu

--- inlämningsuppgift_2/index.js
+++ inlämningsuppgift_2/index.js
@@ -1,25 +1,30 @@
 import PromptSync from "prompt-sync";
-import Band from "./band.js";
-import Musician from "./musician.js";
+//import Band from "./band.js";
+//import Musician from "./musician.js";
+import List from "./bandsAndArtists.js";
 
 const prompt = PromptSync({ sigint: true });
 
 //När vi skapar ett nytt Band och Musicians objekt kommer konstruktorn att läsa in dessa till JSON-filen.
 
-const band = new Band();
-const musicians = new Musician();
+//const band = new Band();
+//const musicians = new Musician();
 
-console.log(band);
-console.log(musicians);
+const bandList = new List(); //kör konstruktorn i klass List
+
+//console.log(band);
+//console.log(musicians);
 
 let running = true;
 
 while (running) {
   console.log(`**Meny** 
 1. Create a new band
-2. Delete a band
-3. Create a new musician
-4. Delete a musician
+2. Remove a band
+3. Show band info
+4. Create a new musician
+5. Remove a musician
+6. Show musician info
 Q. Exit menu
 Select choice from menu ->`);
 
@@ -27,22 +32,41 @@ Select choice from menu ->`);
 
   switch (choice.trim().toUpperCase()) {
     case "1":
-      console.log("Create a new band");
-      band.
+      bandList.addBandtoList(prompt("Vad heter bandet? -> "));
+      // bandList.addBandtoList(prompt("Skriv in info om bandet -> "))
+
+      /*case 1:
+        const tal1 = Number(prompt("Skriv in ett tal: "))
+        const tal2 = Number(prompt("Skriv in ett tal: "))
+        console.log(`$(tal1) + $(tal2) = $(tal1 + tal2)`);
+        */
       break;
+
     case "2":
-      console.log("Delete a band");
-      // band.deleteBand();
-      //band.deleteBand(prompt("Delete a band"));
+      removeBand(prompt("Ta bort ett band"))
       break;
+
+    case "3":
+      console.log("Show Band Info");
+      bandList.showBandInfo()
+      break;
+
     case "3":
       console.log("Create a new musician");
+      addMusician()
       //musicians.createMusicians();
       break;
+
     case "4":
-      console.log("Delete a musician");
+      console.log("Remove a musician");
+      removeMusician()
       //musicians.deleteMusicians();
       //musicians.deleteMusicians(prompt("Delete a musician"));
+      break;
+
+    case "5":
+      console.log("Show Musician Info");
+      removeMusician()
       break;
 
     case "Q":
@@ -52,5 +76,20 @@ Select choice from menu ->`);
     default:
       console.log("Du valde inget giltigt menyval!");
 
+  }
+}
+
+function removeBand() {
+  bandList.showBandInfo();
+  //hundLista.skrivUtHundar(); // Skriver ut listan på alla hundar med index i början.
+  const select = prompt("Skriv in index för det band du vill ta bort ->");
+
+  if (Number(select).toString() === "NaN") { // Kollar så att val går att parsa till ett nummer.
+    console.log("Vänligen skriv in ett tal!");
+  }
+  if (select <= bandList.getLength() && select >= 1) {
+    bandList.removeBandfromList(Number(select) - 1); // Tar det inskrivna valet och minskar med 1. (för arrays index börjar på 0)
+  } else {
+    console.log(`Talet måste vara mellan 1 och ${bandList.getLength()}`);
   }
 }
