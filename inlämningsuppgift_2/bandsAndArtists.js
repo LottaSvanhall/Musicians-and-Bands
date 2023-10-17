5b32fff1a1e90d7f59f1c5b55822faff25830fab
LottaSvanhall <charlotte.svanhall@live.se>
2023-10-17 13:24:38 +0200

    Update bandsAndArtists.js
    Prepared for json to able to read from json,

--- inlämningsuppgift_2/bandsAndArtists.js
+++ inlämningsuppgift_2/bandsAndArtists.js
@@ -2,7 +2,17 @@ import fs from "fs";
 import Band from "./band.js";
 import Musician from "./musician.js";
 
-export default class list {
+export default class List {
 
+  bandList = []; //en array med bandobjekt
+
+  constructor() {
+
+    this.fetchBandData()
+  }
+  fetchBandData() {
+    const data = fs.readFileSync("list.json")
+    this.bandList = JSON.parse(data)
+  }
   
 }
